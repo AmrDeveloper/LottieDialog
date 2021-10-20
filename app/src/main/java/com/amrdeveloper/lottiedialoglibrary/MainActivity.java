e381355023843e4ebd8ba2920b88b3f6124ebb0a
AmrDeveloper <amr96@programmer.net>
2021-10-20 12:46:40 +0200

    Provide 4 examples in app module

--- app/src/main/java/com/amrdeveloper/lottiedialoglibrary/MainActivity.java
+++ app/src/main/java/com/amrdeveloper/lottiedialoglibrary/MainActivity.java
@@ -1,12 +1,11 @@
 package com.amrdeveloper.lottiedialoglibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 import com.amrdeveloper.lottiedialog.LottieDialog;
@@ -18,37 +17,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final LottieDialog dialog = new LottieDialog(this)
+        findViewById(R.id.launch_success_dialog).setOnClickListener(view -> launchSuccessLottieDialog());
+        findViewById(R.id.launch_notification_dialog).setOnClickListener(view -> launchNotificationLottieDialog());
+        findViewById(R.id.launch_location_dialog).setOnClickListener(view -> launchLocationLottieDialog());
+        findViewById(R.id.launch_internet_dialog).setOnClickListener(view -> launchInternetLottieDialog());
+    }
+
+    private void launchSuccessLottieDialog() {
+        Button button = new Button(this);
+        button.setText("Ok");
+        button.setTextColor(Color.WHITE);
+        int greenColor = ContextCompat.getColor(this, R.color.green);
+        button.setBackgroundTintList(ColorStateList.valueOf(greenColor));
+
+        LottieDialog dialog = new LottieDialog(this)
                 .setAnimation(R.raw.success_like)
                 .setAnimationRepeatCount(LottieDialog.INFINITE)
                 .setAutoPlayAnimation(true)
-                .setTitle("LottieDialog Title")
-                .setTitleColor(Color.WHITE)
-                .setTitleTextSize(18)
-                .setMessage("LottieDialog Message")
-                .setMessageColor(Color.WHITE)
-                .setDialogBackground(Color.BLACK)
-                .setMessageTextSize(19)
-                .setCancelable(false)
-                .addActionButton(generateIdenticalButton("Ok1"))
-                .addActionButton(generateIdenticalButton("Ok2"))
-                .addActionButton(generateIdenticalButton("Ok3"))
-                .addActionButton(generateIdenticalButton("Ok4"))
-                .addActionButton(generateIdenticalButton("Ok5"))
-                .setOnShowListener(dialogInterface -> {})
-                .setOnDismissListener(dialogInterface -> {})
-                .setOnCancelListener(dialogInterface -> {});
+                .setDialogBackground(Color.WHITE)
+                .setMessage("Task is Done :D")
+                .setMessageColor(greenColor)
+                .addActionButton(button);
+
         dialog.show();
     }
 
-    public Button generateIdenticalButton(String text) {
+    private void launchNotificationLottieDialog() {
         Button button = new Button(this);
-        button.setText(text);
+        button.setText("See it");
+        button.setTextColor(Color.WHITE);
         button.setAllCaps(false);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            button.setBackgroundTintList(ColorStateList.valueOf(Color.YELLOW));
-        }
-        button.setOnClickListener(view -> Log.d("LottieDialog", "Button click " + text));
-        return button;
+        int purpleColor = ContextCompat.getColor(this, R.color.purple);
+        button.setBackgroundTintList(ColorStateList.valueOf(purpleColor));
+
+        LottieDialog dialog = new LottieDialog(this)
+                .setAnimation(R.raw.purple_notification)
+                .setAnimationRepeatCount(LottieDialog.INFINITE)
+                .setAutoPlayAnimation(true)
+                .setDialogBackground(Color.WHITE)
+                .setMessage("You got a new notification!")
+                .setMessageColor(purpleColor)
+                .addActionButton(button);
+
+        dialog.show();
+    }
+
+    private void launchLocationLottieDialog() {
+        Button button = new Button(this);
+        button.setText("Open GPS");
+        button.setTextColor(Color.WHITE);
+        button.setAllCaps(false);
+        int purpleColor = ContextCompat.getColor(this, R.color.purple);
+        button.setBackgroundTintList(ColorStateList.valueOf(purpleColor));
+
+        LottieDialog dialog = new LottieDialog(this)
+                .setAnimation(R.raw.location)
+                .setAutoPlayAnimation(true)
+                .setAnimationRepeatCount(LottieDialog.INFINITE)
+                .setMessage("Can't get your location")
+                .setMessageColor(purpleColor)
+                .addActionButton(button);
+
+        dialog.show();
+    }
+
+    private void launchInternetLottieDialog() {
+        Button button = new Button(this);
+        button.setText("Retry");
+        button.setTextColor(Color.WHITE);
+        button.setAllCaps(false);
+        int purpleColor = ContextCompat.getColor(this, R.color.green);
+        button.setBackgroundTintList(ColorStateList.valueOf(purpleColor));
+
+        LottieDialog dialog = new LottieDialog(this)
+                .setAnimation(R.raw.no_internet)
+                .setAutoPlayAnimation(true)
+                .setAnimationRepeatCount(LottieDialog.INFINITE)
+                .setMessage("You have no internet connection")
+                .addActionButton(button);
+
+        dialog.show();
     }
 }
